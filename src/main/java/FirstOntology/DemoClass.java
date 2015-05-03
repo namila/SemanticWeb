8f411bd292c46b978ec0551ec7b03b51a34539e9
namilaPerera <dilan.namila@gmail.com>
2015-05-03 21:36:23 +0530

    Departments Created

--- src/main/java/FirstOntology/DemoClass.java
+++ src/main/java/FirstOntology/DemoClass.java
@@ -5,12 +5,50 @@
  */
 package FirstOntology;
 
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.VCARD;
+
 /**
  *
  * @author User
  */
 public class DemoClass {
     public static void main(String[] args) {
+        
+        Model facultyModel=ModelFactory.createDefaultModel();
+        
+        // Uris
+        
+        // Departments
+        String ITDeapartmentUri="http://ITFaculty/Department/IT";
+        String CMDepartmentUri="http://ITFaculty/Department/CM";
+        String ISDepartmentUri="http://ITFaculty/Department/IS";
+               
+        // Resources
+        
+        //Departments
+        Resource ITDepartment=facultyModel.createResource(ITDeapartmentUri);
+        Resource CMDepartment=facultyModel.createResource(CMDepartmentUri);
+        Resource ISDepartment=facultyModel.createResource(ISDepartmentUri);
+       
+        //Setting Properties
+        ITDepartment.addProperty(VCARD.TITLE,"Department Of Information Technology");
+      
+        
+        CMDepartment.addProperty(VCARD.TITLE,"Department Of Computational Mathematics");
+               
+        ISDepartment.addProperty(VCARD.TITLE,"Department Of Interdiciplinary Studies");
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
     
