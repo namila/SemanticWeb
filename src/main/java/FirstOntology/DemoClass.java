2f7f53db41f7d46e2c0f034b01e26bdca2c0aebd
namilaPerera <dilan.namila@gmail.com>
2015-05-03 19:35:12 +0530

    Initial Commit

---
+++ src/main/java/FirstOntology/DemoClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FirstOntology;
+
+/**
+ *
+ * @author User
+ */
+public class DemoClass {
+    public static void main(String[] args) {
+        
+    }
+    
+}
